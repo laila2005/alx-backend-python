73fa5679d2bb6802676ab9427f3b3430b0e9d6d1
Laila Mohamed <87413692+laila2005@users.noreply.github.com>
2025-04-24 15:12:17 +0200

    Update 3-to_str.py

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+""" Basic annotations - to string  """
+
+
 def to_str(n: float) -> str:
-    """function that takes a float n as argument and returns the string representation of the float."""
+    """ Returns the string representation of the float. """
     return str(n)
