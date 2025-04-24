3ab5b9a1093eaafec48848ef03281e0f97ba53bd
Laila Mohamed <87413692+laila2005@users.noreply.github.com>
2025-04-24 15:14:05 +0200

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,29 +1,15 @@
 #!/usr/bin/env python3
-from typing import Callable
+""" Complex types - functions"""
+from typing import Callable, Iterator, Union, Optional, List, Tuple
+
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
-    This function takes a multiplier (a float) as an argument and returns a 
-    function that multiplies a given float by the multiplier.
-    
-    Parameters:
-    multiplier (float): The value by which the input float will be multiplied.
-    
-    Returns:
-    Callable[[float], float]: A function that takes a float as input and returns 
-    the result of multiplying the input by the multiplier.
+    takes a float multiplier as argument,
+    returns a function that multiplies a float by multiplier.
     """
-    
-    def multiplier_func(x: float) -> float:
-        """
-        Multiplies the input value by the multiplier.
+    def f(n: float) -> float:
+        """ multiplies a float by multiplier """
+        return float(n * multiplier)
 
-        Parameters:
-        x (float): The value to be multiplied.
-        
-        Returns:
-        float: The product of x and the multiplier.
-        """
-        return x * multiplier
-
-    return multiplier_func
+    return f
