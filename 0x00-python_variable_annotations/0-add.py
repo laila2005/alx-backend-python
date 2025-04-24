82db7e9b452295f6f32eae7a52e75927525d34a9
Laila Mohamed <87413692+laila2005@users.noreply.github.com>
2025-04-24 15:10:16 +0200

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+
+'''
+    Basic annotations for variables.
+'''
+
+
 def add(a: float, b: float) -> float:
-    """Function that takes a float a and a float b and returns their sum as a float."""
+    '''
+        Returns sum of two floats.
+    '''
     return a + b
