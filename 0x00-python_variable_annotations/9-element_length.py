1052e04d150b15a24f235cfc996c104bffb721fa
Laila Mohamed <87413692+laila2005@users.noreply.github.com>
2025-04-24 15:14:23 +0200

    Update 9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
-from typing import Iterable, Sequence, List, Tuple
+""" Let's duck type an iterable object"""
+from typing import Mapping, MutableMapping, Sequence, Iterable, List, Tuple
+
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """
-    Given an iterable of sequences, returns a list of tuples where each tuple contains
-    the sequence and its length.
-
-    Parameters:
-    lst (Iterable[Sequence]): An iterable of sequences (like lists, strings, etc.).
-
-    Returns:
-    List[Tuple[Sequence, int]]: A list of tuples where each tuple contains a sequence and its length.
-    """
+    """ Element length """
     return [(i, len(i)) for i in lst]
