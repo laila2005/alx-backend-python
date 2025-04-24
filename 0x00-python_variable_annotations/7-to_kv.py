fbdec41f4d35a4e9409a8fde7710ccd23d802572
Laila Mohamed <87413692+laila2005@users.noreply.github.com>
2025-04-24 15:13:47 +0200

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-from typing import Union, Tuple
+""" Complex types - string and int/float to tuple"""
+from typing import Callable, Iterator, Union, Optional, List, Tuple
+
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """function that takes a string k and an int OR float v as arguments and returns a tuple."""
-    return (k, float(v ** 2))
+    """
+    takes a string k and an int OR float v as arguments
+    returns a tuple.
+    """
+
+    return (k, v**2)
